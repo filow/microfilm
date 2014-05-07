55f5650c5239800a84b9829ed7d828bdadf41aa0
JetMuffin <564936642@qq.com>
2014-05-07 22:30:38 +0800

    修改首页海报

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -11,7 +11,7 @@ return array(
     "DB_HOST" 			=> "127.0.0.1",
     "DB_NAME" 			=> "microfilm",
     "DB_USER" 			=> "root",
-    "DB_PWD" 			=> "root",
+    // "DB_PWD" 			=> "root",
     "DB_PORT"			=> "3306",
     "DB_PREFIX" 		=> "mf_",
 
